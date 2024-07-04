9e4fe3e84e9330c43bbf60377e37b30ecfeba90d
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-04 21:38:22 +0800

    feat(eslint): add flat config support and auto-fix on save

--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -1,18 +1,34 @@
+local Lsp = require("utils.lsp")
+
 return {
   {
     "neovim/nvim-lspconfig",
     -- other settings removed for brevity
     opts = {
-      ---@type lspconfig.options
       servers = {
         eslint = {
           settings = {
             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
             workingDirectories = { mode = "auto" },
+            -- change to flag to true to use the flat config (Eslint 9+), refer https://eslint.org/blog/2024/04/eslint-v9.0.0-released/
+            experimental = {
+              useFlatConfig = false,
+            },
           },
         },
       },
-      setup = {},
+      setup = {
+        eslint = function()
+          Lsp.on_attach(function(client, bufnr)
+            if client.name == "eslint" then
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                command = "EslintFixAll",
+              })
+            end
+          end)
+        end,
+      },
     },
   },
 }
