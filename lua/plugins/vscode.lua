73f477c9c38a28ac79fc56a2b8ca2fa6231de2f1
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-10 17:11:05 +0800

    feat: Enhance Neovim keymaps for VSCode functionality

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -23,24 +23,35 @@ end
 vim.api.nvim_create_autocmd("User", {
   pattern = "NvimIdeKeymaps", -- This pattern will be called when the plugin is loaded
   callback = function()
+    -- +File
     -- Find file
     vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+    -- Open other files
+    vim.keymap.set("n", "<leader>,", [[<cmd>call VSCodeNotify('workbench.action.showAllEditors')<cr>]])
     -- Find in files
     vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
-    -- Open symbol
-    vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
     -- Open file explorer in left sidebar
     vim.keymap.set("n", "<leader>e", [[<cmd>call VSCodeNotify('workbench.view.explorer')<cr>]])
+
+    -- +Search
+    -- Open symbol
+    vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+
+    -- +Code
     -- Code Action
     vim.keymap.set("n", "<leader>ca", [[<cmd>call VSCodeNotify('editor.action.codeAction')<cr>]])
+    -- Source Action
+    vim.keymap.set("n", "<leader>cA", [[<cmd>call VSCodeNotify('editor.action.sourceAction')<cr>]])
     -- Code Rename
     vim.keymap.set("n", "<leader>cr", [[<cmd>call VSCodeNotify('editor.action.rename')<cr>]])
+    -- Quickfix shortcut
+    vim.keymap.set("n", "<leader>.", [[<cmd>call VSCodeNotify('editor.action.quickFix')<cr>]])
 
-    -- Terminal
+    -- +Terminal
     -- Open terminal
     vim.keymap.set("n", "<leader>ft", [[<cmd>call VSCodeNotify('workbench.action.terminal.focus')<cr>]])
 
-    -- LSP
+    -- +LSP
     -- View problem
     vim.keymap.set("n", "<leader>xx", [[<cmd>call VSCodeNotify('workbench.actions.view.problems')<cr>]])
     -- Go to next/prev error
@@ -49,18 +60,24 @@ vim.api.nvim_create_autocmd("User", {
     -- LSP Reference, gd is for go to definition but it also works for reference if call it 'gd' on definition :)
     vim.keymap.set("n", "gr", [[<cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<cr>]])
 
-    -- Git
+    -- +Git
     -- Git status
     vim.keymap.set("n", "<leader>gs", [[<cmd>call VSCodeNotify('workbench.view.scm')<cr>]])
     -- Go to next/prev change
     vim.keymap.set("n", "]h", [[<cmd>call VSCodeNotify('workbench.action.editor.nextChange')<cr>]])
     vim.keymap.set("n", "[h", [[<cmd>call VSCodeNotify('workbench.action.editor.previousChange')<cr>]])
 
+    -- +Buffer
     -- Close buffer
     vim.keymap.set("n", "<leader>bd", [[<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<cr>]])
+    -- Close other buffers
+    vim.keymap.set("n", "<leader>bo", [[<cmd>call VSCodeNotify('workbench.action.closeOtherEditors')<cr>]])
 
-    -- Switch project
+    -- +Project
     vim.keymap.set("n", "<leader>fp", [[<cmd>call VSCodeNotify('workbench.action.openRecent')<cr>]])
+
+    -- Other keymaps will be used with https://github.com/VSpaceCode/vscode-which-key, so we don't need to define them here
+    -- Trigger which-key by pressing <CMD+Space>, refer more default keymaps https://github.com/VSpaceCode/vscode-which-key/blob/15c5aa2da5812a21210c5599d9779c46d7bfbd3c/package.json#L265
   end,
 })
 
