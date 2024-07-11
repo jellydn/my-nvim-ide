67752a39116424ca882d5168be9d7dee8d590822
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-12 00:09:33 +0800

    feat: add Tailwind Lsp

---
+++ lua/langs/tailwind.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+          filetypes_include = {},
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
+      },
+    },
+  },
+}
