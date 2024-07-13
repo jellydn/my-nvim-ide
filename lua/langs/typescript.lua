a650e28dd097188b64c314e844b513382f332b5b
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-13 16:16:16 +0800

    feat(test): add vitest

--- lua/langs/typescript.lua
+++ lua/langs/typescript.lua
@@ -265,4 +265,18 @@ return {
       },
     },
   },
+  -- Test
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      vim.list_extend(opts.adapters, {
+        require("neotest-vitest"),
+      })
+    end,
+  },
 }
