4e5950996d9573fe85d4690d1c0ed642a64e984a
Dung Huynh Duc <dunghd.it@gmail.com>
2024-07-28 16:18:22 +0800

    feat(editor): enhance status line display
    feat(editor): Add git status to MiniStatusline

--- lua/core/editor.lua
+++ lua/core/editor.lua
@@ -157,7 +157,32 @@ return {
   -- Status line
   {
     "echasnovski/mini.statusline",
-    opts = { set_vim_settings = false },
+    opts = {
+      set_vim_settings = false,
+      content = {
+        active = function()
+          local MiniStatusline = require("mini.statusline")
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode:upper() } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+            "%<", -- Mark general truncate point
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=", -- End left alignment
+            {
+              hl = "MiniStatuslineFileinfo",
+              strings = {
+                vim.bo.filetype ~= "" and require("mini.icons").get("filetype", vim.bo.filetype),
+              },
+            },
+            { hl = mode_hl, strings = { "%l:%v" } },
+          })
+        end,
+      },
+    },
   },
   -- Remove buffer
   {
