bf96974238b9e60750c9be9bab3fba6028727e55
Dung Huynh Duc <dunghd.it@gmail.com>
2024-07-30 21:36:19 +0800

    feat: enhance lazy.nvim bootstrap and update dependencies
    chore: update lock

--- init.lua
+++ init.lua
@@ -1,10 +1,17 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,6 +34,7 @@ require("lazy").setup({
     -- { import = "plugins.extras.grug-far" },
     -- { import = "plugins.extras.statuscol" },
     -- { import = "plugins.extras.nvim-ufo" },
+    -- { import = "plugins.extras.hardtime" },
     { import = "langs" },
   },
   checker = { enabled = true }, -- automatically check for plugin updates
