ccf7bbf179ff45b5e282369610d3c3bc87602df1
Dung Huynh Duc <dunghd.it@gmail.com>
2024-08-23 20:49:44 +0800

    feat(extras): add nvim-notify plugin for enhanced notifications

---
+++ lua/plugins/extras/nvim-notify.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Dismiss All Notifications",
+      },
+    },
+    opts = {
+      level = vim.log.levels.WARN,
+      stages = "static",
+      render = "minimal",
+      timeout = 1500, -- 1.5s
+      max_height = function()
+        return math.floor(vim.o.lines * 0.40)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.40)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+    init = function()
+      local lazy = require("utils.lazy")
+      if not lazy.has("noice.nvim") then
+        lazy.on_very_lazy(function()
+          vim.notify = require("notify")
+        end)
+      end
+    end,
+  },
+}
---
+++ lua/utils/lazy.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+---@param name string
+function M.get_plugin(name)
+  return require("lazy.core.config").spec.plugins[name]
+end
+
+---@param name string
+---@param path string?
+function M.get_plugin_path(name, path)
+  local plugin = M.get_plugin(name)
+  path = path and "/" .. path or ""
+  return plugin and (plugin.dir .. path)
+end
+
+---@param plugin string
+function M.has(plugin)
+  return M.get_plugin(plugin) ~= nil
+end
+
+---@param fn fun()
+function M.on_very_lazy(fn)
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+      fn()
+    end,
+  })
+end
+
+return M
