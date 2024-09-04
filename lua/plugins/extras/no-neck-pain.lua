5169e4d1e7d9d37589a643580bc655e93bd14464
Dung Huynh Duc <dunghd.it@gmail.com>
2024-09-04 09:55:24 +0800

    fix(no-neck-pain): correct function name and adjust width calculation

--- lua/plugins/extras/no-neck-pain.lua
+++ lua/plugins/extras/no-neck-pain.lua
@@ -1,6 +1,6 @@
-local function zen_mode_width()
+local function calculate_with()
   local width = vim.api.nvim_win_get_width(0)
-  local min_width = math.max(width * 0.75, 90)
+  local min_width = math.max(width * 0.70, 85)
   return math.min(width, min_width)
 end
 
@@ -13,7 +13,7 @@ return {
   {
     "shortcuts/no-neck-pain.nvim",
     opts = {
-      width = zen_mode_width(),
+      width = calculate_with(),
     },
     keys = {
       -- add <leader>cz to enter zen mode
