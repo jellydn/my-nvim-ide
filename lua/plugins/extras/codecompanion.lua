ae4335e70bce26a11aab04e058fa5f8a3a3e8b11
Dung Huynh Duc <dunghd.it@gmail.com>
2024-09-14 16:01:20 +0800

    feat(codecompanion): enhance functionality with new prompts and spinner integration

--- lua/plugins/extras/codecompanion.lua
+++ lua/plugins/extras/codecompanion.lua
@@ -6,7 +6,7 @@ return {
     optional = true,
     opts = {
       spec = {
-        { "<leader>C", group = "Code Companion" },
+        { "<leader>A", group = "AI Code Companion" },
       },
     },
   },
@@ -20,6 +20,8 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      -- Optional to show loading spinner on API request
+      "jellydn/spinner.nvim",
     },
     opts = {
       strategies = {
@@ -67,14 +69,183 @@ return {
       opts = {
         log_level = "DEBUG",
       },
-    },
-    keys = {
-      {
-        "<leader>Ca",
-        "<cmd>CodeCompanionActions<cr>",
-        desc = "Companion actions",
+      pre_defined_prompts = {
+        -- Custom the prompt message
+        ["Generate a Commit Message"] = {
+          prompts = {
+            {
+              role = "user",
+              content = function()
+                return "Write commit message with commitizen convention. Write clear, informative commit messages that explain the 'what' and 'why' behind changes, not just the 'how'."
+                  .. "\n\n```\n"
+                  .. vim.fn.system("git diff")
+                  .. "\n```"
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        -- Add custom prompts
+        ["Generate a Commit Message for Staged"] = {
+          strategy = "chat",
+          description = "Generate a commit message for staged change",
+          opts = {
+            index = 9,
+            mapping = "<LocalLeader>cM",
+            slash_cmd = "staged-commit",
+            auto_submit = true,
+          },
+          prompts = {
+            {
+              role = "user",
+              content = function()
+                return "Write commit message for the change with commitizen convention. Write clear, informative commit messages that explain the 'what' and 'why' behind changes, not just the 'how'."
+                  .. "\n\n```\n"
+                  .. vim.fn.system("git diff --staged")
+                  .. "\n```"
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        ["Document"] = {
+          strategy = "chat",
+          description = "Write documentation for code.",
+          opts = {
+            index = 10,
+            mapping = "<LocalLeader>cd",
+            modes = { "v" },
+            slash_cmd = "doc",
+            auto_submit = true,
+            user_prompt = false,
+            stop_context_insertion = true,
+          },
+          prompts = {
+            {
+              role = "user",
+              content = function(context)
+                local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+                return "Please brief how it works and provide documentation in comment code for the following code. Also suggest to have better naming to improve readability.\n\n```"
+                  .. context.filetype
+                  .. "\n"
+                  .. code
+                  .. "\n```\n\n"
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        ["Review"] = {
+          strategy = "chat",
+          description = "Review the provided code snippet.",
+          opts = {
+            index = 11,
+            mapping = "<LocalLeader>cr",
+            modes = { "v" },
+            slash_cmd = "review",
+            auto_submit = true,
+            user_prompt = false,
+            stop_context_insertion = true,
+          },
+          prompts = {
+            {
+              role = "user",
+              content = function(context)
+                local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+                return "Please review the following code and provide suggestions for improvement then refactor the following code to improve its clarity and readability:\n\n```"
+                  .. context.filetype
+                  .. "\n"
+                  .. code
+                  .. "\n```\n\n"
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        ["Naming"] = {
+          strategy = "chat",
+          description = "Give betting naming for the provided code snippet.",
+          opts = {
+            index = 12,
+            mapping = "<LocalLeader>cn",
+            modes = { "v" },
+            slash_cmd = "naming",
+            auto_submit = true,
+            user_prompt = false,
+            stop_context_insertion = true,
+          },
+          prompts = {
+            {
+              role = "user",
+              content = function(context)
+                local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+                return "Please provide better names for the following variables and functions:\n\n```"
+                  .. context.filetype
+                  .. "\n"
+                  .. code
+                  .. "\n```\n\n"
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
       },
-      { "<leader>Cv", "<cmd>CodeCompanionToggle<cr>", desc = "CopilotChat - Toggle" },
+    },
+    config = function(_, options)
+      require("codecompanion").setup(options)
+
+      -- Show loading spinner when request is started
+      local spinner = require("spinner")
+      local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = "CodeCompanionRequest*",
+        group = group,
+        callback = function(request)
+          if request.match == "CodeCompanionRequestStarted" then
+            spinner.show()
+          end
+          if request.match == "CodeCompanionRequestFinished" then
+            spinner.hide()
+          end
+        end,
+      })
+    end,
+    keys = {
+      -- Recommend setup
+      {
+        "<leader>Aa",
+        "<cmd>CodeCompanionActions<cr>",
+        desc = "Code Companion - Actions",
+        mode = { "n", "v" },
+      },
+      { "<leader>Av", "<cmd>CodeCompanionToggle<cr>", desc = "Code Companion - Toggle", mode = { "n", "v" } },
+      -- Some common usages with visual mode
+      { "<leader>Ae", "<cmd>CodeCompanion /explain<cr>", desc = "Code Companion - Explain code", mode = "v" },
+      { "<leader>Af", "<cmd>CodeCompanion /fix<cr>", desc = "Code Companion - Fix code", mode = "v" },
+      {
+        "<leader>Al",
+        "<cmd>CodeCompanion /lsp<cr>",
+        desc = "Code Companion - Explain LSP diagnostic",
+        mode = { "n", "v" },
+      },
+      { "<leader>At", "<cmd>CodeCompanion /tests<cr>", desc = "Code Companion - Generate unit test", mode = "v" },
+      -- Custom prompts
+      { "<leader>Ad", "<cmd>CodeCompanion /doc<cr>", desc = "Code Companion - Document code", mode = "v" },
+      { "<leader>Ar", "<cmd>CodeCompanion /review<cr>", desc = "Code Companion - Review code", mode = "v" },
+      { "<leader>An", "<cmd>CodeCompanion /naming<cr>", desc = "Code Companion - Better naming", mode = "v" },
     },
   },
 }
