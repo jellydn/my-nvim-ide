8135f5e20acc7074b777c8daf9cca9e1d2de18ea
Huynh Duc Dung <dunghd.it@gmail.com>
2024-09-17 20:40:24 +0800

    feat(fold): add dynamic fold level indicator

--- lua/plugins/extras/fold-preview.lua
+++ lua/plugins/extras/fold-preview.lua
@@ -19,6 +19,15 @@ return {
         fill_char = " ",
         sections = {
           left = {
+            function()
+              -- Calculate the fold level and return the appropriate string
+              local level = vim.v.foldlevel
+              if level == 0 then
+                return "󰁚 "
+              else
+                return string.rep("  ", level - 1) .. "󰁚 "
+              end
+            end,
             "content",
           },
           right = {
