31f4c063034c643f22841d9f567fd7e5946554ea
Dung Huynh Duc <dunghd.it@gmail.com>
2024-09-23 20:43:38 +0800

    refactor(extras): update avante plugin source and clean up comments

--- lua/plugins/extras/avante.lua
+++ lua/plugins/extras/avante.lua
@@ -1,12 +1,10 @@
 return {
-  -- NOTE: This is early/alpha plugin so it's not powerful/stable as CopilotChat.nvim but it's still useful with nice UI
   {
-    "jellydn/avante-copilot.nvim",
+    "yetone/avante.nvim",
     event = "VeryLazy",
     build = "make",
     opts = {
       provider = "copilot", -- You can then change this provider here
-      -- provider = "claude",
       mappings = {
         ask = "<leader>ra",
         edit = "<leader>re",
