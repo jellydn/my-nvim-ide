8b217f752c1d056590861eff2f42d115d267b12e
Dung Huynh Duc <dunghd.it@gmail.com>
2024-11-08 21:00:56 +0800

    feat(vim-test): add support for VSCode integration

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -53,6 +53,7 @@ return {
   },
   {
     "vim-test/vim-test",
+    vscode = true,
     cmd = { "TestNearest", "TestFile", "TestSuite" },
     keys = {
       { "<leader>cjt", "<cmd>TestNearest<cr>", desc = "Run Test Nearest" },
@@ -60,23 +61,26 @@ return {
       { "<leader>cjS", "<cmd>TestSuite<cr>", desc = "Run Test Suite" },
     },
     config = function()
-      local tt = require("toggleterm")
-      local ttt = require("toggleterm.terminal")
+      if vim.g.vscode then
+        vim.g["test#strategy"] = "neovim_vscode"
+      else
+        local tt = require("toggleterm")
+        local ttt = require("toggleterm.terminal")
 
-      vim.g["test#custom_strategies"] = {
-        tterm = function(cmd)
-          tt.exec(cmd)
-        end,
+        vim.g["test#custom_strategies"] = {
+          tterm = function(cmd)
+            tt.exec(cmd)
+          end,
 
-        tterm_close = function(cmd)
-          local term_id = 0
-          tt.exec(cmd, term_id)
-          ttt.get_or_create_term(term_id):close()
-        end,
-      }
+          tterm_close = function(cmd)
+            local term_id = 0
+            tt.exec(cmd, term_id)
+            ttt.get_or_create_term(term_id):close()
+          end,
+        }
 
-      vim.g["test#strategy"] = "tterm"
-
+        vim.g["test#strategy"] = "tterm"
+      end
       -- add -A to deno test
       vim.g["test#javascript#denotest#options"] = "-A"
     end,
