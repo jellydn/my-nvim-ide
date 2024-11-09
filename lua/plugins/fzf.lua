33e20a3035dffceb2e8f37f05281edf63e95d34d
Dung Huynh Duc <dunghd.it@gmail.com>
2024-11-09 15:18:35 +0800

    fix(fzf-lua): remove gr keymap for lsp on neovim 0.11
    Due to
    https://github.com/neovim/neovim/pull/30722#discussion_r1792701875

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -132,10 +132,6 @@ return {
       vim.lsp.handlers["textDocument/declaration"] = fzf_lua.lsp_declarations
       vim.lsp.handlers["textDocument/typeDefinition"] = fzf_lua.lsp_typedefs
       vim.lsp.handlers["textDocument/implementation"] = fzf_lua.lsp_implementations
-      vim.lsp.handlers["textDocument/references"] = function()
-        -- FIXME: This doesn't work on neovim nightly, NVIM v0.11.0-dev-1108+g5a86360400
-        fzf_lua.lsp_references({ ignore_current_line = true, jump_to_single_result = true })
-      end
       vim.lsp.handlers["textDocument/documentSymbol"] = fzf_lua.lsp_document_symbols
       vim.lsp.handlers["workspace/symbol"] = fzf_lua.lsp_workspace_symbols
       vim.lsp.handlers["callHierarchy/incomingCalls"] = fzf_lua.lsp_incoming_calls
