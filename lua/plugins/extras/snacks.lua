46503a795ad8a9c0c78fde0eda09dfdd353fd178
Dung Huynh Duc <dunghd.it@gmail.com>
2024-11-10 17:09:10 +0800

    feat(snacks): improve terminal navigation and window management

--- lua/plugins/extras/snacks.lua
+++ lua/plugins/extras/snacks.lua
@@ -1,3 +1,13 @@
+-- Terminal Mappings
+local function term_nav(dir)
+  ---@param self snacks.terminal
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
+
 return {
   -- Disable toggle term and use Snacks terminal instead
   {
@@ -18,6 +28,28 @@ return {
   {
     "rcarriga/nvim-notify",
     enabled = false,
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    ---@module 'edgy'
+    ---@param opts Edgy.Config
+    opts = function(_, opts)
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "snacks_terminal",
+          size = { height = 0.4 },
+          title = "%{b:snacks_terminal.id}: %{b:term_title}",
+          filter = function(_buf, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+    end,
   },
   {
     "folke/snacks.nvim",
@@ -44,6 +76,16 @@ return {
         lazygit = {
           width = 0,
           height = 0,
+        },
+      },
+      terminal = {
+        win = {
+          keys = {
+            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+          },
         },
       },
     },
@@ -124,6 +166,7 @@ return {
           Snacks.words.jump(vim.v.count1)
         end,
         desc = "Next Reference",
+        mode = { "n", "t" },
       },
       {
         "[[",
@@ -131,6 +174,7 @@ return {
           Snacks.words.jump(-vim.v.count1)
         end,
         desc = "Prev Reference",
+        mode = { "n", "t" },
       },
     },
     init = function()
