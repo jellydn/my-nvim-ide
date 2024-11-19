98105b03a479e250e81aaf49b29c7c2586263a24
Huynh Duc Dung <dunghd.it@gmail.com>
2024-11-19 21:04:03 +0800

    refactor(dashboard): simplify dashboard by removing unused sections

--- lua/plugins/extras/snacks.lua
+++ lua/plugins/extras/snacks.lua
@@ -83,7 +83,6 @@ return {
           header = logo,
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             {
@@ -102,7 +101,6 @@ return {
           { section = "header" },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, limit = 3 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1, limit = 3 },
         },
       },
       bigfile = { enabled = true },
