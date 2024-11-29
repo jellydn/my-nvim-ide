1690642fa16174fc6f191cecc179d70927bf6c0a
Dung Huynh Duc <dunghd.it@gmail.com>
2024-11-29 14:07:23 +0800

    feat(snacks): adjust notifier level to show warnings and above

--- lua/plugins/extras/snacks.lua
+++ lua/plugins/extras/snacks.lua
@@ -115,7 +115,7 @@ return {
         enabled = true,
         --- Available style: "compact"|"fancy"|"minimal"
         style = "fancy", -- similar to the default nvim-notify style
-        level = vim.log.levels.ERROR,
+        level = vim.log.levels.WARN, -- Show only warning and above
       },
       quickfile = { enabled = true },
       statuscolumn = {
