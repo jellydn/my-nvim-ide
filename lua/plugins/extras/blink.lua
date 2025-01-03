d513212273f19e87ead6132ad7c5992bebacf1d4
Dung Huynh Duc <dunghd.it@gmail.com>
2025-01-03 14:47:10 +0800

    feat(blink): add fallback highlight groups for nvim-cmp support

--- lua/plugins/extras/blink.lua
+++ lua/plugins/extras/blink.lua
@@ -65,6 +65,10 @@ return {
         enabled = false,
       },
       appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         nerd_font_variant = "mono",
       },
