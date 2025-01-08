cf9016d79c9969d236fe95d2d6b42a45a920da83
Dung Huynh Duc <dunghd.it@gmail.com>
2025-01-08 09:53:06 +0800

    feat(blink): migrate snippet configuration for v0.10

--- lua/plugins/extras/blink.lua
+++ lua/plugins/extras/blink.lua
@@ -72,22 +72,9 @@ return {
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         nerd_font_variant = "mono",
       },
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
-      },
+      snippets = { preset = "luasnip" },
       sources = {
-        default = { "lsp", "path", "luasnip", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer" },
         -- Disable cmdline completions
         cmdline = {},
       },
