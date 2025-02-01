6b5fae6c9bd79966b1f3b1a4be8f2d22a8c6f14d
Dung Huynh Duc <dunghd.it@gmail.com>
2025-02-01 11:45:18 +0800

    feat(magazine): add configuration for nvim-cmp and luasnip

--- lua/plugins/extras/magazine.lua
+++ lua/plugins/extras/magazine.lua
@@ -15,5 +15,57 @@ return {
       { "iguanacucumber/mag-cmdline", name = "cmp-cmdline" },
       "https://codeberg.org/FelipeLema/cmp-async-path", -- better than cmp-path
     },
+
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup({})
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert({
+          -- Select the [n]ext item
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          -- Select the [p]revious item
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+          -- Scroll the documentation window [b]ack / [f]orward
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+          -- Accept the completion.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+          -- Manually trigger a completion from nvim-cmp.
+          ["<C-Space>"] = cmp.mapping.complete({}),
+
+          -- <c-l> will move you to the right of each of the expansion locations.
+          -- <c-h> is similar, except moving you backwards.
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        }),
+
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+      })
+    end,
   },
 }
