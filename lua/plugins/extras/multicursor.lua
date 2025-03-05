4183982aa48640750f3767c59b21326901cfe261
Dung Huynh Duc <dunghd.it@gmail.com>
2025-03-05 20:51:26 +0800

    refactor(multicursor): change keybinding from Alt+d to Ctrl+m

--- lua/plugins/extras/multicursor.lua
+++ lua/plugins/extras/multicursor.lua
@@ -1,18 +1,13 @@
 return {
   "brenton-leighton/multiple-cursors.nvim",
   opts = {},
+  vscode = false,
   keys = {
     {
-      "<A-d>",
+      "<C-m>",
       "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
       mode = { "n", "x" },
       desc = "Add cursor and jump to next cword",
-    },
-    {
-      "<A-D>",
-      "<Cmd>MultipleCursorsAddDown<CR>",
-      mode = { "n", "x" },
-      desc = "Add cursor and move down",
     },
   },
 }
