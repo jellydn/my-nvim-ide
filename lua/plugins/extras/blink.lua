078ecac43fdc51f4b2f5cead1474820e22b775ef
Dung Huynh Duc <dunghd.it@gmail.com>
2025-03-28 21:13:48 +0800

    feat(blink): add Copilot integration

--- lua/plugins/extras/blink.lua
+++ lua/plugins/extras/blink.lua
@@ -135,4 +135,22 @@ return {
       },
     },
   },
+  -- Support copilot as source
+  {
+    "saghen/blink.cmp",
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
